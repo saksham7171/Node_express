f797261e7ec8f259e19dd060a8202b88b5f9e2cd
saksham <saksham.sharma@tothenew.com>
2016-10-07 11:52:55 +0530

    demo application fnode with express, added middleware too

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+
+var middleware = require("./middleware.js");
+/*app.get('/', function(request, response){
+	response.send('hello express');
+});*/
+
+
+
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAuthentication, function(request, response){
+	response.send('about us');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT,function(){
+	console.log("Server started at port : "+PORT);
+});
