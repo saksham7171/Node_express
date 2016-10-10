aec8d7085f4ef987ca920d789c7bd20198cb034a
saksham <saksham.sharma@tothenew.com>
2016-10-10 10:19:39 +0530

    added todo get apis for listt and single todo

--- server.js
+++ server.js
@@ -2,21 +2,46 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-var middleware = require("./middleware.js");
-/*app.get('/', function(request, response){
-	response.send('hello express');
-});*/
+/*var middleware = require("./middleware.js");
+app.use(middleware.logger);*/
 
+var todos=[{
+	id:1,
+	description: 'Todo 1',
+	completed:false
+}, {
+	id:2,
+	description: 'Todo 2',
+	completed:false
+}, {
+	id:3,
+	description: 'Todo 3',
+	completed:true
+}];
 
+app.get('/todos',function(request,response){
+	response.json(todos);
+});
 
-app.use(middleware.logger);
+app.get('/todo/:id',function(request,response){
+	var todoId = parseInt(request.params.id);
+	var matchedTodo;
 
-app.get('/about', middleware.requireAuthentication, function(request, response){
+	todos.forEach(function(todo){
+		if(todoId === todo.id){
+			matchedTodo=todo;
+		}
+	});
+
+	matchedTodo ? response.json(matchedTodo) : response.status(404).send();
+});
+
+app.get('/about', function(request, response){
 	response.send('about us');
 });
 
 app.use(express.static(__dirname + '/public'));
 
 app.listen(PORT,function(){
-	console.log("Server started at port : "+PORT);
+	console.log("Server started at port : " + PORT);
 });
